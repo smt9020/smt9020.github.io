80955852d6b5d76ea25ad9408695fadd532dfd65
Shannon Trudeau <smtofny@gmail.com>
2015-10-09 13:14:59 -0400

    added some jasmine

---
+++ spec/test.js
@@ -0,0 +1,8 @@
+describe("My Test Suite", function() {
+  it("adds two numbers together", function() {
+    expect(addNumbers(1,2)).toEqual(3);
+  });
+  it("adds two numbers together", function() {
+    expect(addNumbers(2,2)).toEqual(4);
+  });
+});
