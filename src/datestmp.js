d36072750e8f863a818cd25e0b1a7f8ff5a8310c
Shannon Trudeau <smtofny@gmail.com>
2015-11-10 16:06:01 -0500

    adding nav bar stuff

--- src/datestmp.js
+++ src/datestmp.js
@@ -17,12 +17,14 @@ $(document).ready(function(){
         htmlstring = htmlstring + '<li class="list-group-item">' + $("#newTask").val() +'</li>';
         $('#taskList').html(htmlstring);
     })
+    
     $("button").click(function () {
     	var value = 0;
         var num =  $(this).attr('value');
         value = value + +num;
         $('.progress-bar').width(value.toString()+'%'); 
     })
+    
     $("#login").click(function () {
         $('.progress-bar').width('50%'); 
         if (($("#usr").val() == "Shannon") && ($("#pwd").val() == "password123")) {
@@ -33,4 +35,9 @@ $(document).ready(function(){
 
 function addNumbers(num1, num2) {
 	return num1 + num2
+}
+
+function addNavigation() {
+    alert("hi");
+    $('.container').html(navbar.html);
 }
